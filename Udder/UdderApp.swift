4bf1eb5d838767eb8059a01fc7bb542fb218f53f
dev.pnz7 <dev.pnz7@gmail.com>
2023-08-31 21:14:27 +0700

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Panus Chachon on 31/8/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
